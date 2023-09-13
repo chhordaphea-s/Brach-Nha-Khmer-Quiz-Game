fa91e5db5ffcb370228b0d71306618d914ce4cb2
Aing Hongsin <ainghongsin9999@gmail.com>
2023-09-13 16:49:46 +0700

    Create Find possibleAnswer algorithm 	+ create function "getPossiableAnswer(game: Game, level: Int, question: Int)" at Global

--- Brach-Nha-Khmer-Quiz-Game/Globle/Global.swift
+++ Brach-Nha-Khmer-Quiz-Game/Globle/Global.swift
@@ -37,3 +37,32 @@ var answerHint: Int {  return userdefault.integer(forKey: Constant.userdefault.a
 var totalScore: Int { return userdefault.integer(forKey: Constant.userdefault.totalScore) }
 var totalStar: Int { return userdefault.integer(forKey: Constant.userdefault.totalStar) }
 
+
+
+// MARK: FUNCTION
+func getPossiableAnswer(game: Game, level: Int, question: Int) {
+    let correctAns = game.levels[level-1].questions[question-1].answer
+    var allAnswer: [String] = []
+    var possiableAnswwer: [String] = [correctAns]
+    
+    
+    for i in game.levels{
+        for j in i.questions{
+            allAnswer.append(j.answer)
+        }
+    }
+    
+    for _ in 0...2 {
+        var tmpAnswer = allAnswer.randomElement()
+        while game.levels[level-1].questions[question-1].answer == tmpAnswer {
+            tmpAnswer = allAnswer.randomElement()
+        }
+        possiableAnswwer.append(tmpAnswer ?? "")
+
+    }
+    
+    possiableAnswwer.shuffle()
+    
+    print("Possiable Answer: ", possiableAnswwer)
+    print("Answer: ", correctAns)
+}
