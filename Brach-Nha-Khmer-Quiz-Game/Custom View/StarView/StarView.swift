adc9e78aaa1137c56ded04348545a26644c0ea7f
chhordaphea <sophartchhordaphea@gmail.com>
2023-09-25 14:33:45 +0700

    Stars animation and actions for buttons

---
+++ Brach-Nha-Khmer-Quiz-Game/Custom View/StarView/StarView.swift
@@ -0,0 +1,20 @@
+//
+//  StarView.swift
+//  Brach-Nha-Khmer-Quiz-Game
+//
+//  Created by Daphea on 25/9/23.
+//
+
+import UIKit
+
+class StarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
