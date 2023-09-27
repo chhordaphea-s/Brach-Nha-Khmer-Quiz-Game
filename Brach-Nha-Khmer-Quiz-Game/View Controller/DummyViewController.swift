1a0a7e24656eafd6f479f7397e1a78174f903795
chhordaphea <sophartchhordaphea@gmail.com>
2023-09-27 13:45:08 +0700

    create dummy screen and make transition

--- Brach-Nha-Khmer-Quiz-Game/View Controller/DummyViewController.swift
+++ Brach-Nha-Khmer-Quiz-Game/View Controller/DummyViewController.swift
@@ -6,24 +6,30 @@
 //
 
 import UIKit
+import Hero
 
 class DummyViewController: UIViewController {
 
+    @IBOutlet weak var brachNha: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+       
+       
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.switchToHomeScreen()
+        }
+    }
+    
+    func switchToHomeScreen(){
+        let controller = storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+        controller.modalPresentationStyle = .fullScreen
+        controller.modalTransitionStyle = .flipHorizontal
+        
+        self.present(controller, animated: true)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
